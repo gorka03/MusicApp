bf6b5edd673d23f4f19e75c8ccba02edfaac27a4
in2dm3-v <gorka.delaiglesiape@elorrieta-errekamari.com>
2024-10-03 17:24:29 +0200

    Push final MusicAppXML

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.musicapp
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageButtonTelefono: ImageButton
+
+    private var isImageOriginal = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        imageButtonTelefono = findViewById(R.id.imageButtonPausar)
+        imageButtonTelefono.setOnClickListener {
+            if (isImageOriginal) {
+                imageButtonTelefono.setImageResource(android.R.drawable.ic_media_play)
+            } else {
+                imageButtonTelefono.setImageResource(android.R.drawable.ic_media_pause)
+            }
+            isImageOriginal = !isImageOriginal
+        }
     }
 }
